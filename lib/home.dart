a1a0557c911eb015749773c5bdc4625cde56c3af
K4MiR <cleberrobson16@gmail.com>
2022-10-17 21:56:08 -0300

    foi adicionado ajustes na lista e AlertDialog para ações do usuário

--- lib/home.dart
+++ lib/home.dart
@@ -8,20 +8,16 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   List _itens = [];
 
-  void _carregarItens(){
-    for(int i=0; i<=10; i++) {
+  void _carregarItens() {
+    for (int i = 0; i <= 10; i++) {
       Map<String, dynamic> item = Map();
       item["título"] = "Título ${i} Lorem ipsum dolor sit amet";
       item["descrição"] = "Descrição ${i} Lorem ipsum dolor sit amet";
       _itens.add(item);
-
     }
-
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -35,17 +31,47 @@ class _HomeScreenState extends State<HomeScreen> {
         padding: EdgeInsets.all(20),
         child: ListView.builder(
             itemCount: _itens.length,
-            itemBuilder: (context, indice)  {
-              Map<String, dynamic> item = _itens[indice] ;
+            itemBuilder: (context, indice) {
+              Map<String, dynamic> item = _itens[indice];
               //print("intem ${ _itens[indice]["título"] }");
 
               return ListTile(
-                title: Text( _itens[indice]["título"]),
+                onTap: () {
+                  //print("clique com onTap ${indice}");
+                  showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          title: Text("Deseja deletar?"),
+                          titlePadding: EdgeInsets.all(20),
+                          content: Text(_itens[indice]["descrição"],),
+                          contentPadding: EdgeInsets.all(20),
+                          actions: [
+                            TextButton(
+                                onPressed: (){
+                                  print("Selecionado sim");
+                                  Navigator.pop(context);
+                                },
+                                child: Text("Sim")
+                            ),
+                            TextButton(
+                                onPressed: (){
+                                  print("Selecionado não");
+                                  Navigator.pop(context);
+                                },
+                                child: Text("Não")
+                            ),
+                          ],
+                        );
+                      });
+                },
+                // onLongPress: (){
+                //   print("clique com onLongPress");
+                // },
+                title: Text(_itens[indice]["título"]),
                 subtitle: Text(_itens[indice]["descrição"]),
-
               );
-            }
-            ),
+            }),
       ),
     );
   }
